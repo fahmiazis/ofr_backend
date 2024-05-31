5ec7cebba5a0bf79bf2327aa4d02a49701677fef
fahmiazis <fahmiazis797@gmail.com>
2024-05-31 10:47:56 +0700

    modified:   src/controllers/tarif.js

--- src/controllers/tarif.js
+++ src/controllers/tarif.js
@@ -380,6 +380,9 @@ module.exports = {
       }
       if (!limit) {
         limit = 10
+      } else if (limit === 'all') {
+        const findLimit = await veriftax.findAll()
+        limit = findLimit.length
       } else {
         limit = parseInt(limit)
       }
